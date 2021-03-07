262eb8f23a40eb105e234b82d49bdb90fd7d96cf
dalgarno <32097481+dalgarno@users.noreply.github.com>
2021-03-07 15:25:06 +0000

    feature: convert to baseline risk calculator

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask_restx import Resource, Api
+from flask import Flask, request
+from flask_restx import Resource, Api, fields
 
 from pathlib import Path
 import json
@@ -7,39 +7,58 @@ import json
 app = Flask(__name__)
 api = Api(app)
 
-COUNTRY_PATH = Path('./country_data.json')
-COUNTRY_DATA = json.loads(COUNTRY_PATH.read_text())
 
-COUNTRY_CODES = set()
-PER_COUNTRY_DATA = {}
-
-for entry in COUNTRY_DATA:
-    if 'Country_code' not in entry:
-        continue
-    country_code = entry['Country_code']
-    COUNTRY_CODES.add(country_code)
-    if country_code not in PER_COUNTRY_DATA:
-        PER_COUNTRY_DATA[country_code] = [entry]
-    else:
-        PER_COUNTRY_DATA[country_code].append(entry)
-
-COUNTRY_CODES = sorted([c for c in COUNTRY_CODES])
+bmi_category_to_risk = {
+    "UNDERWEIGHT": 3,
+    "HEALTHY": 1,
+    "OVERWEIGHT": 2,
+    "OBESE": 3,
+    "VERY_OBESE": 4,
+}
 
 
-@api.route('/country_codes')
-class CountryCodes(Resource):
-    def get(self):
-        return COUNTRY_CODES
+age_bracket_to_risk = {
+    "group_1": 1,
+    "group_2": 1,
+    "group_3": 2,
+    "group_4": 4,
+    "group_5": 5,
+}
 
 
-@api.route('/country/<string:country_code>')
-class Country(Resource):
-    def get(self, country_code):
-        if country_code not in PER_COUNTRY_DATA:
-            return {'message': f'Country code {country_code} not found!'}, 404
-        return PER_COUNTRY_DATA[country_code]
+def compute_risk(bmi_category, underlying_health_issues, age_bracket):
+    health_factor = 3 if underlying_health_issues else 0
+
+    return (
+        bmi_category_to_risk[bmi_category] * age_bracket_to_risk[age_bracket]
+        + health_factor
+    )
 
 
+resource_fields = api.model(
+    "Resource",
+    {
+        "bmi_category": fields.String,
+        "underlying_health_issues": fields.Boolean,
+        "age_group": fields.String,
+    },
+)
 
-if __name__ == '__main__':
+
+@api.route("/baseline_risk")
+class BaseLine(Resource):
+    @api.expect(resource_fields)
+    def post(self):
+        body = request.get_json(force=True)
+
+        bmi_category = body["bmi_category"]
+        underlying_health_issues = body["underlying_health_issues"]
+        age_group = body["age_group"]
+
+        risk_category = compute_risk(bmi_category, underlying_health_issues, age_group)
+
+        return {"risk_category": min(risk_category, 10)}
+
+
+if __name__ == "__main__":
     app.run()
